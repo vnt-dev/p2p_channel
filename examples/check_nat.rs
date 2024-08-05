f02dae4977276037898528ecc3dfe7c74c28a338
xmh0511 <970252187@qq.com>
2024-08-05 15:27:04 +0800

    更新example和注释

--- examples/check_nat.rs
+++ examples/check_nat.rs
@@ -1,4 +1,15 @@
 fn main() {
-    let r = p2p_channel::stun::stun_test_nat(vec!["193.43.148.37:3478".to_string()], None).unwrap();
+    let r = p2p_channel::stun::stun_test_nat(
+        vec![
+            "stun.miwifi.com:3478".to_string(),
+            "stun.chat.bilibili.com:3478".to_string(),
+            "stun.hitv.com:3478".to_string(),
+            "stun.cdnbye.com:3478".to_string(),
+            "stun.tel.lu:3478".to_string(),
+            "stun.smartvoip.com:3478".to_string(),
+        ],
+        None,
+    )
+    .unwrap();
     println!("{:?}", r);
 }
